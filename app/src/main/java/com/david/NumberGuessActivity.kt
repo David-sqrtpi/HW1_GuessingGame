a540a03073a071cb11f81c0eeb7b5862ce2b153c
David-sqrtpi <david.stiven911@gmail.com>
2021-09-01 15:46:06 -0500

    Global variables

--- app/src/main/java/com/david/NumberGuessActivity.kt
+++ app/src/main/java/com/david/NumberGuessActivity.kt
@@ -11,50 +11,63 @@ import androidx.core.view.isVisible
 import kotlin.random.Random
 
 class NumberGuessActivity : AppCompatActivity() {
+    var randomNumber = 0
+    var attempts = 0
 
-    var randomNumber: Int = 0
-    var attempts: Int = 0
+    var editText: EditText? = null
+    var congrats: TextView? = null
+    var reset: Button? = null
+    var guess: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_number_guess)
-        attempts = 0
-        randomNumber = Random.nextInt(0, 1000)
+
+        randomNumber = Random.nextInt(1, 1000)
+
+        editText = findViewById<EditText>(R.id.editText)
+        congrats = findViewById<TextView>(R.id.congrats)
+        reset = findViewById<Button>(R.id.resest)
+        guess = findViewById<Button>(R.id.guessButton)
     }
 
-    fun guess(view: View){
-        val editText = findViewById<EditText>(R.id.editText)
-        val congrats = findViewById<TextView>(R.id.congrats)
+    fun guess(view: View) {
         try {
-            val enteredNumber = editText.text.toString().toInt()
+            val enteredNumber = editText?.text.toString().toInt()
 
             attempts++
-            congrats.isVisible = true
-            congrats.text = "Intentos: $attempts"
+            congrats?.isVisible = true
+            congrats?.text = "Intentos: $attempts"
 
-            if (enteredNumber > randomNumber) {
-                Toast.makeText(this,"El número aleatorio es menor", Toast.LENGTH_LONG).show()
-            } else if (enteredNumber < randomNumber) {
-                Toast.makeText(this,"El número aleatorio es mayor", Toast.LENGTH_LONG).show()
-            } else {
-                congrats.text = "Felicitaciones, has adivinado el número $randomNumber usando $attempts intentos"
-                editText.isEnabled = false
-                view.isEnabled = false
-                val reset = findViewById<Button>(R.id.resest)
-                reset.isVisible = true
+            when {
+                enteredNumber > randomNumber -> {
+                    Toast.makeText(this, "El número aleatorio es menor", Toast.LENGTH_LONG).show()
+                }
+                enteredNumber < randomNumber -> {
+                    Toast.makeText(this, "El número aleatorio es mayor", Toast.LENGTH_LONG).show()
+                }
+                else -> {
+                    congrats?.text = "Felicitaciones, has adivinado el número $randomNumber usando $attempts intentos"
 
-                reset.setOnClickListener { _->
-                    reset.isVisible = false
-                    congrats.isVisible = false
-                    editText.isEnabled = true
-                    editText.setText("")
-                    view.isEnabled = true
-                    attempts = 0
-                    randomNumber = Random.nextInt(0, 1000)
+                    editText?.isEnabled = false
+                    view.isEnabled = false
+                    reset?.isVisible = true
                 }
             }
-        } catch(e: Exception){
-            Toast.makeText(this,"Ingresa un número", Toast.LENGTH_LONG).show()
+
+        } catch (e: Exception) {
+            Toast.makeText(this, "Ingresa un número", Toast.LENGTH_LONG).show()
         }
+    }
+
+    fun reset(view: View) {
+        reset?.isVisible = false
+        congrats?.isVisible = false
+        editText?.isEnabled = true
+        editText?.setText("")
+        view.isEnabled = true
+        attempts = 0
+        randomNumber = Random.nextInt(0, 1000)
+        guess?.isEnabled = true
     }
 }
